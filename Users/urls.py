d9acf7e42ed45fcacd721a9a7b6a9035dfbeb460
YonatanK <yonatank@radware.com>
2023-01-29 18:50:05 +0200

    update

--- Users/urls.py
+++ Users/urls.py
@@ -10,10 +10,11 @@ urlpatterns = [
     path('home/', views.userHome, name="user-home"),
     path('account/', views.userAccount, name="account"),
     path('admin-page/', views.userAccount, name="admin-page"),
+
     # path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerUser, name="register"),
     # path('account/', views.home, name="account"),
 
-    # path('customer/', views.customer, name="customer"),
+    path('customers/', views.customers, name="customers"),
     # path('', views.customers, name="customers"),
 ]
--- maintenance/urls.py
+++ maintenance/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path('maintenances/', views.maintenances, name='maintenances'),
     path('maintenance/<str:pk>/', views.maintenance, name='maintenance_category'),
-    path('request/', views.create_request, name='request'),
+    path('create_request/', views.create_request, name='create_request'),
     path('request_details/<str:pk>/', views.request_details, name='request_details'),
+    path('requests/', views.requests, name='requests'),
     path('IT/', views.helpDesk, name='IT'),
 ]
